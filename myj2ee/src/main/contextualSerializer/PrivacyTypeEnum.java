b415900193ff0bc722239fd573e38524bed1396f
caichengcheng <caichengcheng@zhihu.com>
2023-01-09 11:40:28 +0800

    脱敏

---
+++ myj2ee/src/main/contextualSerializer/PrivacyTypeEnum.java
@@ -0,0 +1,24 @@
+package contextualSerializer;
+
+import lombok.Getter;
+
+/**
+ * 隐私数据类型枚举
+ */
+@Getter
+public enum PrivacyTypeEnum {
+    /** 自定义（此项需设置脱敏的范围）*/
+    CUSTOMER,
+
+    /** 姓名 */
+    NAME,
+
+    /** 身份证号 */
+    ID_CARD,
+
+    /** 手机号 */
+    PHONE,
+
+    /** 邮箱 */
+    EMAIL,
+}
